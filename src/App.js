550382ed4d71d375a0358eea69e9cce5d382c046
sunny-rajak <sunnyrajak257@gmail.com>
2024-11-25 23:26:56 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useMemo, useState } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [weight, setWeight] = useState(50);
+    const [height, setHeight] = useState(150);
+
+    const handelWeightChange = (e) => {
+        setWeight(e.target.value);
+    };
+
+    const handelHeightChange = (e) => {
+        setHeight(e.target.value);
+    };
+
+    const bmiValue = useMemo(() => {
+        const calcHeight = height / 100;
+
+        return (weight / Math.pow(calcHeight, 2)).toFixed(2);
+    }, [weight, height]);
+
+    const bmiMessage = useMemo(() => {
+        const bmi = parseFloat(bmiValue);
+        switch (true) {
+            case bmi < 18.5:
+                return "You are underweight.";
+            case bmi >= 18.5 && bmi < 24.9:
+                return "You have a normal weight.";
+            case bmi >= 24.9 && bmi < 29.9:
+                return "You are overweight.";
+            default:
+                return "You are obese.";
+        }
+    }, [bmiValue]);
+
+    return (
+        <main>
+            <h1>BMI Calculator</h1>
+            <div className="input-section">
+                <p className="slider-output">Weight: {weight} kg</p>
+                <input
+                    className="input-slider"
+                    type="range"
+                    step="1"
+                    min="20"
+                    max="200"
+                    value={weight}
+                    onChange={handelWeightChange}
+                />
+                <p className="slider-output">Height: {height}cm</p>
+                <input
+                    className="input-slider"
+                    type="range"
+                    step="1"
+                    min="100"
+                    max="250"
+                    value={height}
+                    onChange={handelHeightChange}
+                />
+            </div>
+            <div className="output-section">
+                <p>Your BMI is</p>
+                <p className="output-bmi">{bmiValue}</p>
+                <p className="bmi-message">{bmiMessage}</p>
+            </div>
+        </main>
+    );
+};
 
 export default App;
